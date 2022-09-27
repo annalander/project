cb7d5eeb51fb9540eb1deb0bb2f5638da375b4c7
Anna Lander <anna.lander@selu.edu>
2022-09-27 20:26:20 +0000

    jkgjkb

--- scripts/grouped.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-
-butterfly_data <- read_csv("../data/Butterfly_data.csv")
-
-grouped_data <- function(dataset = butterfly_data){
-  new_butterfly_data <- butterfly_data %>% 
-  return(new_butterfly_data)
-}
